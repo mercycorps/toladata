8d95d730b659ebb6f1dd36135b2e046d846f45a6
Cameron Mcfee <cameron.mcfee@gmail.com>
2019-09-03 22:35:59 -0700

    cleanup and more unicode fixes

--- tola/l10n_utils.py
+++ tola/l10n_utils.py
@@ -6,35 +6,38 @@ from django.utils import formats
 import six
 import decimal
 
-def _date_format(date, format):
-    return formats.date_format(
+def _date_format(date, date_format, decode=False):
+    formatted = formats.date_format(
         date,
-        format,
+        date_format,
         use_l10n=True
-    ).encode('UTF-8')
+    )
+    if decode:
+        return formatted
+    return formatted.encode('UTF-8')
 
 
-def l10n_date_iso(date):
-    return _date_format(date, 'DATE_FORMAT')
+def l10n_date_iso(date, decode=False):
+    return _date_format(date, 'DATE_FORMAT', decode=decode)
 
 
-def l10n_date_short(date):
-    return _date_format(date, 'SHORT_DATE_FORMAT')
+def l10n_date_short(date, decode=False):
+    return _date_format(date, 'SHORT_DATE_FORMAT', decode=decode)
 
 
-def l10n_date_medium(date):
-    return _date_format(date, 'MEDIUM_DATE_FORMAT')
+def l10n_date_medium(date, decode=False):
+    return _date_format(date, 'MEDIUM_DATE_FORMAT', decode=decode)
 
 
-def l10n_date_long(date):
-    return _date_format(date, 'LONG_DATE_FORMAT')
+def l10n_date_long(date, decode=False):
+    return _date_format(date, 'LONG_DATE_FORMAT', decode=decode)
 
 
-def l10n_date_year_month(date):
-    return _date_format(date, 'YEAR_MONTH_FORMAT')
+def l10n_date_year_month(date, decode=False):
+    return _date_format(date, 'YEAR_MONTH_FORMAT', decode=decode)
 
-def l10n_monthname(date):
-    return _date_format(date, 'N')
+def l10n_monthname(date, decode=False):
+    return _date_format(date, 'N', decode=decode)
 
 def l10n_number(value):
     if isinstance(value, (decimal.Decimal, float) + six.integer_types):
