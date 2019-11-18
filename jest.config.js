b3c2fdbcec349af0ebdda519c81537c3db1edd6b
Cameron Mcfee <cameron.mcfee@gmail.com>
2019-11-18 09:03:28 -0800

    js test infrastructure

--- jest.config.js
+++ jest.config.js
@@ -71,7 +71,10 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "@root/(.*)$": "<rootDir>/js/$1",
+    "@components/(.*)$": "<rootDir>/js/components/$1"
+    },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
