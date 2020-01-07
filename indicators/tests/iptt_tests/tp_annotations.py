70743039f68857911eb0555e0b9a5297b573a300
Cameron Mcfee <cameron.mcfee@gmail.com>
2020-01-07 13:56:10 -0800

    checking in work

--- indicators/tests/iptt_tests/tp_annotations.py
+++ indicators/tests/iptt_tests/tp_annotations.py
@@ -11,7 +11,19 @@ from indicators.models import Indicator
 
 class TestLopActuals(test.TestCase):
     def setUp(self):
+        country = w_factories.CountryFactory(country="Testland", code="TL")
         self.program = w_factories.RFProgramFactory()
+        disaggregation = i_factories.DisaggregationTypeFactory(
+            country=country,
+            standard=False
+        )
+        self.categories = [
+            i_factories.DisaggregationLabelFactory(
+                disaggregation_type=disaggregation,
+                label="Test Label {}".format(c),
+                customsort=c+1
+            ) for c in range(5)
+        ]
 
     def get_tp_indicator(self, indicator, frequency):
         return IPTTIndicator.timeperiods.filter(pk=indicator.pk).with_frequency_annotations(
