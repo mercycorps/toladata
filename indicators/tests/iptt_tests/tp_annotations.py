293f1cd2e940e35caff526ce1bb3f4ef06d2f13d
Cameron Mcfee <cameron.mcfee@gmail.com>
2020-01-10 16:54:38 -0800

    started functional tests

--- indicators/tests/iptt_tests/tp_annotations.py
+++ indicators/tests/iptt_tests/tp_annotations.py
@@ -1,3 +1,4 @@
+import unittest
 from django import test
 
 from factories import (
@@ -8,35 +9,23 @@ from factories import (
 from indicators.queries import IPTTIndicator
 from indicators.models import Indicator
 
+from indicators.tests.iptt_tests.scenarios import IPTTReportProgram
 
+
+unittest.skip("re-implementing")
 class TestLopActuals(test.TestCase):
     def setUp(self):
-        country = w_factories.CountryFactory(country="Testland", code="TL")
-        self.program = w_factories.RFProgramFactory()
-        disaggregation = i_factories.DisaggregationTypeFactory(
-            country=country,
-            standard=False
-        )
-        self.categories = [
-            i_factories.DisaggregationLabelFactory(
-                disaggregation_type=disaggregation,
-                label="Test Label {}".format(c),
-                customsort=c+1
-            ) for c in range(5)
-        ]
+        self.iptt_program = IPTTReportProgram()
 
-    def get_tp_indicator(self, indicator, frequency):
-        return IPTTIndicator.timeperiods.filter(pk=indicator.pk).with_frequency_annotations(
-            frequency, self.program.reporting_period_start, self.program.reporting_period_end
-        ).first()
-
-    def test_lop_only_lop_actuals(self):
-        indicator = i_factories.RFIndicatorFactory(
-            program=self.program,
-            target_frequency=Indicator.LOP,
-            targets=1000,
-            results=1000
-            )
-        tp_indicator = self.get_tp_indicator(indicator, Indicator.LOP)
-        self.assertEqual(tp_indicator.lop_actual, 1000)
-        
+    def test_no_results(self):
+        self.iptt_program.add_indicators_with_no_results()
+        report_data = self.iptt_program.get_tp_report()
+        self.assertEqual(len(report_data), 7)
+        for indicator_report_data in report_data:
+            self.assertEqual(indicator_report_data['lop_actual'], None)
+            self.assertIn('disaggregated_report_data', indicator_report_data)
+            self.assertEqual(len(indicator_report_data['disaggregated_report_data']), 2)
+            for disaggregation in self.iptt_program.disaggregations:
+                self.assertIn("{}".format(disaggregation.pk), indicator_report_data['disaggregated_report_data'])
+                disaggregation_report_data = indicator_report_data['disaggregated_report_data']["{}".format(disaggregation.pk)]
+                self.assertEqual(disaggregation_report_data['lop_actual'], None)
