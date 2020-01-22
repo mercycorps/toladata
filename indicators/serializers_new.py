f2f8fbe5fad662b4ed82fba14df75b1b44232912
Cameron Mcfee <cameron.mcfee@gmail.com>
2020-01-21 18:13:29 -0800

    moves lop disaggregated data back to initial query, uses that output to only annotate disaggregation categories that are actually in use for the disaggregations period data query (reducing query times) also commenting, docstrings, and linting

--- indicators/serializers_new.py
+++ indicators/serializers_new.py
@@ -4,7 +4,7 @@ import string
 import operator
 from decimal import Decimal
 from rest_framework import serializers
-from indicators.models import Indicator, Level, LevelTier, DisaggregationType, DisaggregationLabel
+from indicators.models import Indicator, Level, LevelTier, DisaggregationLabel
 from indicators.queries import IPTTIndicator
 from workflow.models import Program
 from tola.model_utils import get_serializer
@@ -68,7 +68,8 @@ class IndicatorBase:
             return str(string.ascii_lowercase[indicator.level_order])
         elif indicator.level_id and indicator.level_order and indicator.level_order >= 26:
             return str(
-                string.ascii_lowercase[indicator.level_order/26 - 1] + string.ascii_lowercase[indicator.level_order % 26]
+                string.ascii_lowercase[indicator.level_order/26 - 1] +
+                string.ascii_lowercase[indicator.level_order % 26]
                 )
         return None
 
@@ -277,6 +278,7 @@ class IPTTReportIndicatorMixin:
 
     @classmethod
     def load_report(cls, program_id, frequency):
+        """Primary entry point into serializer, prefetches, annotates, runs query, returns data"""
         program_data = Program.rf_aware_objects.only(
             'pk', 'reporting_period_start', 'reporting_period_end'
         ).get(pk=program_id)
@@ -285,27 +287,36 @@ class IPTTReportIndicatorMixin:
         ).prefetch_related(None).order_by().filter(
             disaggregation_type__indicator__program_id=program_id
         ).distinct().values_list('pk', flat=True)
-        indicators = cls.get_queryset(program_id, frequency).with_frequency_annotations(
+        indicators = cls.get_queryset(program_id, frequency).with_disaggregation_lop_annotations(
+            disaggregation_categories
+        ).with_frequency_annotations(
             frequency, program_data.reporting_period_start, program_data.reporting_period_end,
         )
+        # get the disaggregation categories _actually_ in use (lop_actual isn't 0)
+        active_disaggregations = list(set(
+            category_pk for indicator in indicators for category_pk in indicator.active_disaggregation_category_pks
+        ))
+        # disaggregation period annotations broken out into their own query due to MySQL temp table size limit:
         disaggregated_indicators = cls.get_disaggregations_queryset(
             program_id, frequency
-        ).with_disaggregation_lop_annotations(disaggregation_categories).with_disaggregation_frequency_annotations(
+        ).with_disaggregation_frequency_annotations(
             frequency, program_data.reporting_period_start, program_data.reporting_period_end,
-            disaggregations=disaggregation_categories
+            disaggregations=active_disaggregations,
         )
-        return cls(indicators, many=True,
-                   context={'frequency': frequency, 'disaggregated_indicators': {di.pk: di for di in disaggregated_indicators}}
-                    ).data
+        return cls(indicators, many=True, context={
+            'frequency': frequency,
+            'disaggregated_indicators': {di.pk: di for di in disaggregated_indicators}}
+                   ).data
 
     def get_disaggregated_data(self, indicator):
+        """lop_actual value for each disaggregation category assigned to the indicator"""
         return {
-            disaggregation_pk: {
-                'lop_actual': getattr(self.context['disaggregated_indicators'][indicator.pk], 'disaggregation_{}_lop_actual'.format(disaggregation_pk))
-            }
-        for disaggregation_pk in indicator.disaggregation_category_pks}
+            disaggregation_pk: {'lop_actual': getattr(indicator, f'disaggregation_{disaggregation_pk}_lop_actual')}
+            for disaggregation_pk in indicator.disaggregation_category_pks
+        }
 
     def get_report_data(self, indicator):
+        """period data for each period in frequency (actual for TP, actual/target/% met for TvA)"""
         count = getattr(indicator, 'frequency_{0}_count'.format(self.context.get('frequency')), 0)
         report_data = self.period_serializer_class(
             [self.get_period_data(indicator, c) for c in range(count)], many=True
@@ -313,6 +324,7 @@ class IPTTReportIndicatorMixin:
         return report_data
 
     def get_disaggregated_period_data(self, indicator, disaggregation_pk, count):
+        """disaggregated period data - only TP (actuals) data currently spec'd - may include targets later"""
         return {
             'index': count,
             'actual': getattr(
@@ -325,16 +337,19 @@ class IPTTReportIndicatorMixin:
             }
 
     def get_disaggregated_report_data(self, indicator):
+        """disaggregated period data for each period in frequency, only actuals, uses get_disaggregated_period_data"""
         count = getattr(indicator, 'frequency_{0}_count'.format(self.context.get('frequency')), 0)
         # note: filter here means only the periods with values and the final period (null or not) get sent
         # this is to minimize the size of the JSON transfers going back and forth
         disaggregated_report_data = {
-            category_pk: self.disaggregated_period_serializer_class(
+            **{category_pk: self.disaggregated_period_serializer_class(
                 filter(
                     lambda period_data: (period_data['index'] == count-1 or period_data['actual'] is not None),
                     [self.get_disaggregated_period_data(indicator, category_pk, c) for c in range(count)]
                 ), many=True
-            ).data for category_pk in indicator.disaggregation_category_pks}
+                ).data for category_pk in indicator.active_disaggregation_category_pks},
+            **{category_pk: [{'index': count-1, 'actual': None}] for category_pk in indicator.inactive_disaggregation_category_pks}
+        }
         return disaggregated_report_data
 
 
@@ -348,7 +363,7 @@ class TVAPeriod(serializers.Serializer):
 class TimeperiodsPeriod(serializers.Serializer):
     index = serializers.IntegerField()
     actual = DecimalDisplayField()
-    
+
 
 class IPTTTVAMixin:
     period_serializer_class = TVAPeriod
@@ -362,7 +377,7 @@ class IPTTTVAMixin:
         return IPTTIndicator.tva.filter(
             program_id=program_id, target_frequency=frequency
         )
-    
+
     @classmethod
     def get_disaggregations_queryset(cls, program_id, frequency):
         return IPTTIndicator.timeperiods.filter(
