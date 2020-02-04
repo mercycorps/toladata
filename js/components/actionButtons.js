e9a0b39df744fab318147e301c8114b6bf49d96f
Paul Souders <paul@axoplasm.com>
2020-02-04 11:56:56 -0800

    add icons to expando toggles #2112

--- js/components/actionButtons.js
+++ js/components/actionButtons.js
@@ -5,6 +5,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import { faPlusSquare, faMinusSquare } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faPlusSquare, faMinusSquare)
+
 
 library.add(faPlusSquare, faMinusSquare);
 
