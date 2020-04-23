e25f544595bbce940f8585434411d1aa1091b0a5
Cameron Mcfee <cameron.mcfee@gmail.com>
2020-04-22 18:10:31 -0700

    uses login/logout signals to add request session vars as needed

--- tola/signals.py
+++ tola/signals.py
@@ -1,21 +1,22 @@
+import logging
+from importlib import import_module
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 from django.conf import settings
-from importlib import import_module
 
 SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
 
+logger = logging.getLogger(__name__)
 
-@receiver(user_logged_in)
+@receiver(user_logged_in, dispatch_uid="global_user_login_signal")
 def sig_user_logged_in(sender, user, request, **kwargs):
-    #get user profile data to set session vars
-    #user_profile = UserProfile.objects.all().filter(user=request.user).values('country')
-    #request.session['country'] = user_profile.country
+    logger.info("user logged in: %s at %s", user, request.META.get('REMOTE_ADDR', "No address in request"))
+    # set covid banner on user session (dismissable by closing)
+    request.session['show_covid_banner'] = True
 
-    logger = logging.getLogger(__name__)
-    logger.info("user logged in: %s at %s" % (user, request.META['REMOTE_ADDR']))
-
-@receiver(user_logged_out)
+@receiver(user_logged_out, dispatch_uid="global_user_logout_signal")
 def sig_user_logged_out(sender, user, request, **kwargs):
-    logger = logging.getLogger(__name__)
-    logger.info("user logged out: %s at %s" % (user, request.META['REMOTE_ADDR']))
+    logger.info("user logged out: %s at %s", user, request.META.get('REMOTE_ADDR', "No address in request"))
+    # remove any session variables set that should be reinstated on login
+    if 'show_covid_banner' in request.session:
+        del request.session['show_covid_banner']
