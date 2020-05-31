160646b00ba8cc5909b62b76724a1284b644a555
Sanjuro Jogdeo <sjogdeo@mercycorps.org>
2020-05-30 20:12:30 -0700

    For mercycorps/TolaActivity#2042, relax template switching some more.

--- js/base.js
+++ js/base.js
@@ -262,7 +262,7 @@ $(function() {
     $('[data-toggle="popover"]').on('click', function(e){
         e.preventDefault();
     });
-    
+
     /* specific actions tied to item-specific site-wide events here: */
     // in case of covid alert dismissal, update the session to clear it for the rest of this login session:
     $('#covid-banner-alert').on('close.bs.alert', function() {
@@ -706,7 +706,7 @@ window.create_unified_changeset_notice = ({
     })
 }
 
-const createPnotifyAlert = (passedInConfig) => {
+const   createPnotifyAlert = (passedInConfig) => {
     let config = {
         textTrusted: true,
         icon: false,
