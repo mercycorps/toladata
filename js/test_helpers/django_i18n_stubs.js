90b210392edde6a97f4b4c2bcd4547ba9381761c
Cameron Mcfee <cameron.mcfee@gmail.com>
2020-08-28 16:34:27 -0700

    test repair

--- js/test_helpers/django_i18n_stubs.js
+++ js/test_helpers/django_i18n_stubs.js
@@ -5,7 +5,7 @@
 
 window.gettext = jest.fn(s => s);
 window.ngettext = jest.fn(s => s);
-window.pgettext = jest.fn(s => s);
+window.pgettext = jest.fn((c, s) => s);
 window.npgettext = jest.fn(s => s);
 window.interpolate = jest.fn(s => s);
 
