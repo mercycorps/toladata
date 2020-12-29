13726a4db117812e274c8a3650639d086c510452
Cameron Mcfee <cameron.mcfee@gmail.com>
2020-12-29 13:40:42 -0800

    update url to have trailing slash as per rest of the site

--- workflow/urls.py
+++ workflow/urls.py
@@ -11,7 +11,7 @@ from django.urls import path
 
 urlpatterns = [
     path('siteprofile_list/<int:program_id>/<int:activity_id>/', SiteProfileList.as_view(), name='siteprofile_list'),
-    path('siteprofile_add', SiteProfileCreate.as_view(), name='siteprofile_add'),
+    path('siteprofile_add/', SiteProfileCreate.as_view(), name='siteprofile_add'),
     path('siteprofile_update/<int:pk>/', SiteProfileUpdate.as_view(), name='siteprofile_update'),
     path('siteprofile_delete/<int:pk>/', SiteProfileDelete.as_view(), name='siteprofile_delete'),
 
