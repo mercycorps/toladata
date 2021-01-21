0645b0d0ead302ffe7642ad4fec1867cff66252b
Andre Thomas <anthomas@mercycorps.org>
2021-01-21 11:28:05 -0800

    Updated code structure.

--- js/pages/iptt_report/components/sidebar/sidebar.js
+++ js/pages/iptt_report/components/sidebar/sidebar.js
@@ -9,12 +9,12 @@ export default () => {
                     <IPTTFilterForm />
                 </div>
                 <div className="sidebar-toggle">
-                <a href="#" data-target="#sidebar" data-toggle="collapse"
-                        title={
-                            /* # Translators: A toggle button that hides a sidebar of filter options */
-                            gettext('Show/Hide Filters') }>
-                    <i className="fa fa-chevron-left"></i>
-                </a>
+                    <a href="#" data-target="#sidebar" data-toggle="collapse"
+                            title={
+                                /* # Translators: A toggle button that hides a sidebar of filter options */
+                                gettext('Show/Hide Filters') }>
+                        <i className="fa fa-chevron-left"></i>
+                    </a>
                 </div>
             </div>
         </div>
