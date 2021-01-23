bbb35eb9103383941469ce56ca25b147ce6279a3
Cameron Mcfee <cameron.mcfee@gmail.com>
2021-01-22 16:29:05 -0800

    commenting and test repair

--- tola/middleware.py
+++ tola/middleware.py
@@ -30,20 +30,29 @@ class UserLanguageMiddleware(object):
 
 
 class FailModeMiddleware(object):
+    """On QA / localhost, this middleware will fail all ajax/json requests if the session key "fail_mode" is set"""
 
     def __init__(self, get_response):
+        """Initial configuration"""
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
-        if not request.user or not request.user.is_superuser:
-            return response
-        if (not request.is_ajax() and 'application/json' not in request.META['HTTP_ACCEPT']):
-            return response
+        # fail mode only applies on local and QA, where DEBUG=true, if debug isn't true (prod failsafe) bail:
         if not settings.DEBUG:
             return response
+        # fail mode only applies to superusers, so if there isn't a user or they are not a superuser, bail:
+        if not request.user or not request.user.is_superuser:
+            return response
+        # fail mode isn't supposed to interrupt regular page loads, only ajax / json requests:
+        if (not request.is_ajax() and
+            'application/json' not in getattr(getattr(request, 'META', {}), 'HTTP_ACCEPT', [])):
+            return response
+        # if they haven't activated fail mode, don't fail:
         if not request.session.get('fail_mode', False):
             return response
+        # allow requests to deactivate fail mode:
         if request.path == '/fail_mode_toggle':
             return response
+        # fail the request:
         return HttpResponseServerError("Fail Mode Activated")
