92dabdcd30480e975827e4467c6c4cad6e0180c6
Andre Thomas <anthomas@mercycorps.org>
2021-01-27 09:49:56 -0800

    Added some more comments for bottom scrolling.

--- js/pages/iptt_report/components/report/reportBody.js
+++ js/pages/iptt_report/components/report/reportBody.js
@@ -7,14 +7,15 @@ import ReportTableBody from './tableBody';
 export default ({ setBottomScrolling }) => {
 
     useEffect(() => {
+        /* Adding a listener to track the position on page scrolling. Scrollable is the visible area a user can scroll the page calculated by the total height of page minus height of the visible window minus height of the footer. */
         window.addEventListener('scroll', () => {
             let footer = document.querySelector("#footer").offsetHeight;
             let scrollable = document.documentElement.scrollHeight - window.innerHeight - footer;
-            setBottomScrolling(scrollable - window.scrollY) // Where the sidebar should match scrolling
+            setBottomScrolling(scrollable - window.scrollY) // Setting how much space left a user can scroll to reach the bottom.
         })
     }, []);
 
-    return <main className="iptt_table_wrapper" >
+    return <main className="iptt_table_wrapper">
                 <div id="id_div_top_iptt_report">
                     <IPTTHeader />
                     <table className="table table-sm table-hover table__iptt" id="iptt_table">
--- js/pages/iptt_report/components/sidebar/sidebar.js
+++ js/pages/iptt_report/components/sidebar/sidebar.js
@@ -5,11 +5,13 @@ export default ({bottomScrolling}) => {
 
     const [startScroll, setStartScroll] = useState(null);
 
+    /* At the bottom of the page, if the amount of space left for a user to scroll the IPTT report is less than or equal to the amount of the sidebar that is not visible, the sidebar scroll position will be set to match the scroll position of the page creating a sync scrolling effect. */
     if (startScroll && bottomScrolling && bottomScrolling <= startScroll) {
         let sidebarElement = document.querySelector("#sidebar");
         sidebarElement.scrollTop = startScroll - bottomScrolling;
     }
 
+    // Calculate how much of the sidebar is not visible and can be scrolled.
     useEffect(() => {
         let sidebarElement = document.querySelector("#sidebar");
         let start = sidebarElement.scrollHeight - sidebarElement.clientHeight;
