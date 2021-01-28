4987a253428890c5e96d4035c2c3420bb84f5240
Andre Thomas <anthomas@mercycorps.org>
2021-01-27 19:15:09 -0800

    Finish update to handle when scrolling reaches the footer, to keep access to top of the sidebar.

--- js/pages/iptt_report/components/sidebar/sidebar.js
+++ js/pages/iptt_report/components/sidebar/sidebar.js
@@ -4,7 +4,6 @@ import IPTTFilterForm from './filterForm';
 export default ({bottomScrolling, extend}) => {
 
     const [startScroll, setStartScroll] = useState(null);
-    const [open, setOpen] = useState(true);
 
     /* At the bottom of the page, if the amount of space left for a user to scroll the IPTT report is less than or equal to the amount of the sidebar that is not visible, the sidebar scroll position will be set to match the scroll position of the page creating a sync scrolling effect. */
     if (startScroll && bottomScrolling && bottomScrolling <= startScroll) {
@@ -16,27 +15,24 @@ export default ({bottomScrolling, extend}) => {
         let sidebarElement = document.querySelector("#sidebar");
         let start = sidebarElement.scrollHeight - sidebarElement.clientHeight;
         setStartScroll(start)
-        // document.querySelector("a").addEventListener('click', () => {
-        //     console.log(!sidebarElement.classList.contains('show'))
-        //     setOpen(sidebarElement.classList.contains('show'))
-        // })
     }, [])
     
     return (
         <div className="sidebar_container">
             <div className="sidebar_wrapper">
                 <div className="collapse width show" id="sidebar">
-                <div className="extender-main" style={{height: extend >= 0 ? (extend + 10) + "px" : 0}}></div>
+                <div className="extender-main" style={{height: extend >= 0 ? extend : 0}}></div>
                     <IPTTFilterForm />
                 </div>
                 <div className="sidebar-toggle">
-                    <div className="extender-toggle" style={{height: open && extend >= 0 ? extend + "px" : 0}}></div>
-
                     <a href="#" data-target="#sidebar" data-toggle="collapse"
-                            title={
-                                /* # Translators: A toggle button that hides a sidebar of filter options */
-                                gettext('Show/Hide Filters') }>
-                        <i className="fa fa-chevron-left"></i>
+                        title={
+
+                            /* # Translators: A toggle button that hides a sidebar of filter options */
+                            gettext('Show/Hide Filters') }>
+
+                            <div className="extender-toggle" style={{height: extend >= 0 ? extend : 0}}></div>
+                            <i className="fa fa-chevron-left"></i>
                     </a>
                 </div>
             </div>
