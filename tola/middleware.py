946953cc8dae53e183640c3eb0191c4e37e5d6c0
Cameron Mcfee <cameron.mcfee@gmail.com>
2021-01-28 10:53:59 -0800

    debug doesn't apply on qa, moved check to get host and reordered to minimize every-request overhead

--- tola/middleware.py
+++ tola/middleware.py
@@ -37,20 +37,31 @@ class FailModeMiddleware(object):
         self.get_response = get_response
 
     def __call__(self, request):
+        """Attempts to bail out of fail mode as early as possible, to minimize overhead on every request.
+
+            Ordered by likelihood:
+                - not a superuser
+                - fail mode isn't on
+                - not on qa or local (placed third because of the get_host() overhead)
+                - not an ajax / json request
+                - not trying to get to the fail mode toggle to turn it off
+        """
         response = self.get_response(request)
-        # fail mode only applies on local and QA, where DEBUG=true, if debug isn't true (prod failsafe) bail:
-        if not settings.DEBUG:
-            return response
         # fail mode only applies to superusers, so if there isn't a user or they are not a superuser, bail:
         if not request.user or not request.user.is_superuser:
+            return response
+        # if they haven't activated fail mode, don't fail:
+        if not request.session.get('fail_mode', False):
+            return response
+        # if we aren't on qa or local, fail mode doesn't even apply:
+        uri = request.get_host()
+        if not any(["qa" in uri, "127." in uri, "local" in uri]):
             return response
         # fail mode isn't supposed to interrupt regular page loads, only ajax / json requests:
         if (not request.is_ajax() and
             'application/json' not in getattr(request, 'META', {}).get('HTTP_ACCEPT', [])):
             return response
-        # if they haven't activated fail mode, don't fail:
-        if not request.session.get('fail_mode', False):
-            return response
+
         # allow requests to deactivate fail mode:
         if request.path == '/fail_mode_toggle':
             return response
