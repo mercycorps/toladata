2937a71f887a29e30c26737dc14e4ab31b85d51b
Andre Thomas <anthomas@mercycorps.org>
2021-02-01 15:43:19 -0800

    Fixed typo in boxShadow.

--- js/components/selectWidgets.js
+++ js/components/selectWidgets.js
@@ -166,7 +166,7 @@ export const MultiSelectCheckbox = ( props ) => {
                 ...provided,
                 backgroundColor: '#f5f5f5',
                 border: "1px solid #b8babd",
-                boxShadow: "0 1px 1px 1px rgba(0, 0, 0, 0.08",
+                boxShadow: "0 1px 1px 1px rgba(0, 0, 0, 0.08)",
             }),
     };
     const formatOptionLabel = (props) => {
