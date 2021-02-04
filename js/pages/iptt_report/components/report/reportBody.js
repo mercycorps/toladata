06b728106d6841d9c15f8eec72e444304ae820e5
Andre Thomas <anthomas@mercycorps.org>
2021-02-04 13:30:03 -0800

    Added comments to explain sidebar scroll syncing effect.

--- js/pages/iptt_report/components/report/reportBody.js
+++ js/pages/iptt_report/components/report/reportBody.js
@@ -7,26 +7,29 @@ import ReportTableBody from './tableBody';
 export default ({ setBottomScrolling, setExtend, setTableHeight }) => {
 
     useEffect(() => {
-        /* Adding a function to track the position on page scrolling. Scrollable is the visible area a user can scroll the page calculated by the total height of page minus height of the visible window minus height of the footer. */
+        /* A function to track key variables when the IPTT is scrolled. */
         const scrollEventHandler = () => {
             let footer = document.querySelector("#footer").offsetHeight;
+
+            // The "scrollable" variable is the non visible height of the IPTT table (the height remaining to be scrolled). It is calculated by taking the total height of the page and subtracting the height of the visible portion of the browser and height of the footer.
             let scrollable = document.documentElement.scrollHeight - window.innerHeight - footer;
             
-            // Setting the total height of the table. Set time out needed to render the sidebar in correct order when changing programs, otherwise the sidebar's scroll Top will be off.
+            // Setting how much space left a user can scroll before reaching the bottom of the IPTT. 
+            setBottomScrolling(parseInt(scrollable - window.scrollY));
+
+            // Setting the total height of the table. SetTimeout needed to render the sidebar in correct order when changing programs, otherwise the sidebar's scroll Top will be off.
             setTimeout(() => {
                 setTableHeight(scrollable);
             }, 1)
 
-            // Setting how much space left a user can scroll to reach the bottom.
-            setBottomScrolling(parseInt(scrollable - window.scrollY));
-
-            // Setting the offset height of the footer to extend the sidebar content.
+            // Setting the offset height of the footer to extend the sidebar content. This is the amount/height that the footer is visible.
             setExtend(parseInt(footer - (document.documentElement.scrollHeight - window.innerHeight - window.scrollY)));
         }
 
-        // Event listeners for page scrolling and resizing
+        // Event listeners to track page scrolling and resizing, including zooming.
         window.onresize = scrollEventHandler;
         window.onscroll = scrollEventHandler;
+
     }, []);
 
     return <main className="iptt_table_wrapper">
