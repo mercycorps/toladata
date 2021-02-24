187e6cbf159987e102e849560ed403d58ca13580
Cameron Mcfee <cameron.mcfee@gmail.com>
2021-02-24 11:41:07 -0800

    cleaned up unneeded imports from an init

--- indicators/tests/serializer_tests/__init__.py
+++ indicators/tests/serializer_tests/__init__.py
@@ -1,18 +1,1 @@
-# from indicators.tests.serializer_tests.indicator_base_serializer import *
-# from indicators.tests.serializer_tests.indicator_with_measurement_serializer import *
-# from indicators.tests.serializer_tests.program_page_indicator_serializer import *
-# from indicators.tests.serializer_tests.program_base_serializer import *
-# from indicators.tests.serializer_tests.program_level_ordering_program_serializer import *
-# from indicators.tests.serializer_tests.rf_level_ordering_program_serializer import *
-# from indicators.tests.serializer_tests.program_page_program_serializer import *
-# from indicators.tests.serializer_tests.program_page_endpoint import *
-# from indicators.tests.serializer_tests.iptt_qs_program_serializer import *
-# from indicators.tests.serializer_tests.iptt_indicator_serializer import *
-# from indicators.tests.serializer_tests.iptt_program_serializer import *
-# from indicators.tests.serializer_tests.iptt_endpoints import *
-# from indicators.tests.serializer_tests.iptt_report_serializer import *
 
-
-# None of these tests in this folder are named test*.py, so I'm not sure that any of them are running right now.
-# iptt_report_serializer isn't because it has a couple of bad imports that should cause tests to fail.  What
-# are these tests for?
