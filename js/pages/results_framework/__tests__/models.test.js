d1ac6c49769ac15d0921d76e00c5411144a7f22c
Andre Thomas <anthomas@mercycorps.org>
2021-04-05 15:14:44 -0700

    Update test suite.

--- js/pages/results_framework/__tests__/models.test.js
+++ js/pages/results_framework/__tests__/models.test.js
@@ -22,11 +22,12 @@ describe ('Test for when there is no RF level saved', function() {
     it('Users should see Choose RF Template Carefully warning', async function() {
         await axios.get.mockResolvedValue({
             data: {
-                result: true
+                data: true
             }
         })
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
         await runAllPromises();
+        expect(wrapper.state('show_import_banner')).toBe(true)
         expect(wrapper.exists('.level-list-panel')).toBe(true);
         expect(wrapper.exists('.level-list-panel__dingbat')).toBe(true);
         expect(wrapper.exists('.level-list-panel__text')).toBe(true);
@@ -34,45 +35,49 @@ describe ('Test for when there is no RF level saved', function() {
     it('Users should not see the level list component', async function() {
         await axios.get.mockResolvedValue({
             data: {
-                result: true
+                data: true
             }
         })
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
         await runAllPromises();
+        expect(wrapper.state('show_import_banner')).toBe(true)
         expect(wrapper.exists('#level-list')).toBe(false);
     })
 
     it('Users with Low Access should not see Bulk Import Banner', async function() {
         await axios.get.mockResolvedValue({
             data: {
-                result: true
+                data: true
             }
         })
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
         await runAllPromises();
         rootStore.levelStore.accessLevel = "low"
+        expect(wrapper.state('show_import_banner')).toBe(true)
         expect(wrapper.exists('#bulk-import-banner-alert')).toBe(false);
     })
     it('Users with Medium Access should not see Bulk Import Banner', async function() {
         await axios.get.mockResolvedValue({
             data: {
-                result: true
+                data: true
             }
         })
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
         await runAllPromises();
         rootStore.levelStore.accessLevel = "medium"
+        expect(wrapper.state('show_import_banner')).toBe(true)
         expect(wrapper.exists('#bulk-import-banner-alert')).toBe(false);
     })
     it('Users with High Access should not see Bulk Import Banner', async () => {
         await axios.get.mockResolvedValue({
             data: {
-                result: true
+                data: true
             }
         })
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
         await runAllPromises();
         rootStore.levelStore.accessLevel = "high"
+        expect(wrapper.state('show_import_banner')).toBe(true)
         expect(wrapper.exists('#bulk-import-banner-alert')).toBe(false);
     })
 });
@@ -87,27 +92,16 @@ describe ('Test for when there is at least one RF level saved', function() {
     })
 
     const runAllPromises = () => new Promise(setImmediate)
-    
-    it('Test the api call', async () => {
-        await axios.get.mockResolvedValue({
-            data: {
-                result: true
-            }
-        })
-        let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
-        await runAllPromises();
-        expect(wrapper.exists('#bulk-import-banner-alert')).toBe(true);
-        expect(wrapper.state('show_import_banner')).toEqual(true)
-    })
 
     it('Users should not see Choose RF Template Carefully warning', async function() {
         await axios.get.mockResolvedValue({
             data: {
-                result: true
+                data: true
             }
         })
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
         await runAllPromises();
+        expect(wrapper.state('show_import_banner')).toBe(true)
         expect(wrapper.exists('.level-list-panel')).toBe(false);
         expect(wrapper.exists('.level-list-panel__dingbat')).toBe(false);
         expect(wrapper.exists('.level-list-panel__text')).toBe(false);
@@ -115,62 +109,67 @@ describe ('Test for when there is at least one RF level saved', function() {
     it('Users should see the level list component', async function() {
         await axios.get.mockResolvedValue({
             data: {
-                result: true
+                data: true
             }
         })
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
         await runAllPromises();
+        expect(wrapper.state('show_import_banner')).toBe(true)
         expect(wrapper.exists('#level-list')).toBe(true);
     })
 
     it('Users with Low Access should not see Bulk Import Banner', async function() {
         await axios.get.mockResolvedValue({
             data: {
-                result: true
+                data: true
             }
         })
         rootStore.levelStore.accessLevel = "low"
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
-        await runAllPromises();        
+        await runAllPromises();   
+        expect(wrapper.state('show_import_banner')).toBe(true)     
         expect(wrapper.exists('#bulk-import-banner-alert')).toBe(false);
     })
     it('Users with Medium Access should not see Bulk Import Banner', async function() {
         await axios.get.mockResolvedValue({
             data: {
-                result: true
+                data: true
             }
         })
         rootStore.levelStore.accessLevel = "medium"
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
         await runAllPromises();
+        expect(wrapper.state('show_import_banner')).toBe(true)
         expect(wrapper.exists('#bulk-import-banner-alert')).toBe(false);
     })
     it('Users with High Access should see Bulk Import Banner', async function() {
         await axios.get.mockResolvedValue({
             data: {
-                result: true
+                data: true
             }
         })
         rootStore.levelStore.accessLevel = "high"
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
         await runAllPromises();
+        expect(wrapper.state('show_import_banner')).toBe(true)
         expect(wrapper.exists('#bulk-import-banner-alert')).toBe(true);
     })
     it('Users with High Access should not see Bulk Import Banner if previously closed', async function() {
         await axios.get.mockResolvedValue({
             data: {
-                result: false
+                data: false
             }
         })
         rootStore.levelStore.accessLevel = "high"
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
         await runAllPromises();
+        expect(wrapper.state('show_import_banner')).toBe(false)
         expect(wrapper.exists('#bulk-import-banner-alert')).toBe(false);
     })
     it('User should see the banner, click close, and then does not see the banner', async function() {
         await axios.get.mockResolvedValue({
             data: {
-                result: true
+                data: true
             }
         })
         axios.put.mockResolvedValue({
@@ -178,6 +177,7 @@ describe ('Test for when there is at least one RF level saved', function() {
         })
         let wrapper = await shallow(<LevelListPanel.wrappedComponent rootStore={rootStore}/>);
         await runAllPromises();
+        expect(wrapper.state('show_import_banner')).toBe(true)
         expect(wrapper.exists('#bulk-import-banner-alert')).toBe(true);
         const closeButton = wrapper.find('#bulk-import-banner-alert').find('button');
         closeButton.simulate('click');
