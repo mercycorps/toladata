19dbb3bbe0d8246172aca73fd1c5942c250268be
Sanjuro Jogdeo <sjogdeo@mercycorps.org>
2021-04-06 17:49:26 -0700

    for mercycorps/TolaActivity#2047 update temporary translations to have diacritics, update sorting of strings without diacritics.

--- tola/db_translations.py
+++ tola/db_translations.py
@@ -1,33 +1,33 @@
 from django.utils.translation import ugettext_lazy as _
 
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string0 = _("Annual")
+string0 = _("By distribution")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string1 = _("Quarterly")
+string1 = _("Endline")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string2 = _("Endline")
+string2 = _("Post shock")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string3 = _("Midline")
+string3 = _("Quarterly")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string4 = _("Weekly")
+string4 = _("By training")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string5 = _("By training")
+string5 = _("Final evaluation")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string6 = _("Semi-annual")
+string6 = _("By batch")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string7 = _("By distribution")
+string7 = _("Weekly")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string8 = _("By batch")
+string8 = _("Semi-annual")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string9 = _("Post shock")
+string9 = _("Baseline")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string10 = _("By event")
+string10 = _("Annual")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
 string11 = _("Monthly")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string12 = _("Final evaluation")
+string12 = _("Midline")
 # Translators: One of several options for specifying how often data is collected or reported on over the life of a program
-string13 = _("Baseline")
+string13 = _("By event")
 # Translators: One of several choices for what sector (i.e. development domain) a program is most closely associated with
 string14 = _("Agribusiness")
 # Translators: One of several choices for what sector (i.e. development domain) a program is most closely associated with
--- tola/management/commands/create_temp_translations.py
+++ tola/management/commands/create_temp_translations.py
@@ -1,9 +1,10 @@
 
 from os import path
+import re
+import itertools
 import polib
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from indicators.models import Sector, IndicatorType, ReportingFrequency, DataCollectionFrequency
 
 
 class Command(BaseCommand):
@@ -32,19 +33,30 @@ class Command(BaseCommand):
         file_root = path.join(settings.DJANGO_ROOT, 'locale')
         po_file_obj = polib.pofile(path.join(file_root, partial_path))
         print('\nProcessing', partial_path)
+
         untranslated_entries = [entry for entry in po_file_obj.untranslated_entries() if not entry.obsolete]
+        white_space_regex = re.compile('(^\s+(?:\<.+?>)?)(.+)', re.DOTALL)
+
         if untranslated_entries:
             print(f'Creating {len(untranslated_entries)} temporary strings for untranslated entries')
+            diacritic_cycle = itertools.cycle(['Á', 'é', '', '', 'Ź'])
             for entry in untranslated_entries:
-                entry.msgstr = 'Translated ' + entry.msgid
+                if matches := white_space_regex.match(entry.msgid):
+                    entry.msgstr = f'{matches.group(1)}{next(diacritic_cycle)}Translated {matches.group(2)}'
+                else:
+                    entry.msgstr = f'{next(diacritic_cycle)}Translated {entry.msgid}'
         else:
             print('No untranslated strings found')
 
         fuzzy_entries = [entry for entry in po_file_obj.fuzzy_entries() if not entry.obsolete]
         if fuzzy_entries:
             print(f'Creating {len(fuzzy_entries)} temporary strings for fuzzy entries')
+            diacritic_cycle = itertools.cycle(['Á', 'é', '', '', 'Ź'])
             for entry in fuzzy_entries:
-                entry.msgstr = 'Translated ' + entry.msgid
+                if matches := white_space_regex.match(entry.msgid):
+                    entry.msgstr = f'{matches.group(1)}{next(diacritic_cycle)}Translated {matches.group(2)}'
+                else:
+                    entry.msgstr = f'{next(diacritic_cycle)}Translated {entry.msgid}'
                 entry.flags.remove('fuzzy')
         else:
             print('No fuzzy strings found')
