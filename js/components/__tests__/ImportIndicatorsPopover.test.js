fb876cd19e3f03aedb1a2c6c08433f008a13d05a
Andre Thomas <anthomas@mercycorps.org>
2021-05-03 13:57:37 -0700

    Updated tests.

--- js/components/__tests__/ImportIndicatorsPopover.test.js
+++ js/components/__tests__/ImportIndicatorsPopover.test.js
@@ -11,12 +11,18 @@ describe('Import Indicators test suite', () => {
                 tiers: ["Goal", "Outcome", "Output", "Activity"],
             }
         },
-        chosenTierSetKey: "mc_standard"
+        chosenTierSetKey: "mc_standard",
     }
     let chosenTiers = ["Goal", "Outcome", "Output", "Activity"];
+    let tierLevelsUsed = [
+        {name: "Goal", used: true}, 
+        {name: "Outcome", used: true}, 
+        {name: "Output", used: true}, 
+        {name: "Activity", used: false},
+    ]
 
     it('The Import indicators button should contain the right components', () => {
-        let wrapper = mount(<ImportIndicatorsPopover program_id={123} chosenTiers={chosenTiers} />)
+        let wrapper = mount(<ImportIndicatorsPopover program_id={123} tierLevelsUsed={tierLevelsUsed} />)
         expect(wrapper.exists('.importIndicators-body')).toBe(true);
         expect(wrapper.exists('.btn-upload')).toBe(true);
         expect(wrapper.exists('.btn-download')).toBe(true);
