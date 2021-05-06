409d2cc53d5f1992aef1f577fd3b55764146d204
Sanjuro Jogdeo <sjogdeo@mercycorps.org>
2021-05-06 10:31:15 -0700

    For mercycorps/TolaActivity#2574, update option 3.

--- indicators/management/commands/create_participant_count_indicators.py
+++ indicators/management/commands/create_participant_count_indicators.py
@@ -8,38 +8,33 @@ from workflow.models import Country, Program, Organization
 
 class Command(BaseCommand):
     help = """
-        Setup targets for indicators by reading a CSV file
+        Create participant count country, programs, and indicators
         """
+
+    def add_arguments(self, parser):
+        parser.add_argument('--clean_option3_indicators', action='store_true')
 
     def handle(self, *args, **options):
         option1_country_names = ['Lebanon', 'Palestine (West Bank / Gaza)', 'Indonesia']
-        option3_country_names = ['Liberia', 'Mali']
+        option3_countries = {'Liberia': 7, 'Mali': 6}
+        option3_program_name_template = 'Participant Count {}'
+
         country_programs = {}
         pc_country_name = 'Mapping Our Reach'
         program_start_date = date(2020, 7, 1)
         program_end_date = date(2022, 6, 30)
         mc_org = Organization.objects.get(name="Mercy Corps")
-
-        for country_name in option1_country_names + option3_country_names:
-            country_programs[country_name] = Program.objects.filter(country__country=country_name, funding_status='Funded')
-
-        pc_country, created = Country.objects.get_or_create(country=pc_country_name, defaults={
-            'organization': mc_org,
-            'code': "ZZ"
-        })
-        if created:
-            print("Dummy Mapping Our Reach country created")
-        else:
-            print("Dummy Mapping Our Reach country already existed")
-
         metrics = {
             'indicator_created': {}, 'indicator_existed': {}, 'program_existed': 0, 'program_created': 0,
             'dates_created': 0, 'rf_created': 0
         }
 
+        if options['clean_option3_indicators']:
+            self.clean_option3(option3_countries, option3_program_name_template)
+
         # First do option1 - an indicator in each real program in each real country in the option 1 list.
         for country_name in option1_country_names:
-            for program in country_programs[country_name]:
+            for program in Program.objects.filter(country__country=country_name, funding_status='Funded'):
                 try:
                     top_level = Level.objects.get(parent=None, program=program)
                 except Level.DoesNotExist:
@@ -58,14 +53,28 @@ class Command(BaseCommand):
                     except KeyError:
                         metrics['indicator_existed'][country_name] = 1
 
-        # The do option2 - a dummy country with one dummy program per participating country and an
+        # Now do option3 - a dummy country with one dummy program per participating country and an
         # indicator for each program in that country.
-        for country_name in option3_country_names:
-            dummy_program, created = Program.objects.get_or_create(name=f'Participant Count {country_name}', defaults={
-                'funding_status': 'Funded',
-                'reporting_period_start': program_start_date,
-                'reporting_period_end': program_end_date,
-            })
+
+        pc_country, created = Country.objects.get_or_create(country=pc_country_name, defaults={
+            'organization': mc_org,
+            'code': "ZZ"
+        })
+        if created:
+            print("Dummy Mapping Our Reach country created")
+        else:
+            print("Dummy Mapping Our Reach country already existed")
+
+        for country, indicator_count in option3_countries.items():
+            country_program_name = option3_program_name_template.format(country)
+            dummy_program, created = Program.objects.get_or_create(
+                name=country_program_name,
+                defaults={
+                    'funding_status': 'Funded',
+                    'reporting_period_start': program_start_date,
+                    'reporting_period_end': program_end_date,
+                }
+            )
             if created:
                 dummy_program.country.add(pc_country)
                 metrics['program_created'] += 1
@@ -84,19 +93,19 @@ class Command(BaseCommand):
                     LevelTier.objects.create(name=tier_name, program=dummy_program, tier_depth=i+1)
                 metrics['rf_created'] += 1
 
-            for real_program in country_programs[country_name]:
-                indicator_name = f'Number of people reached in fiscal year in {real_program.name}'
+            for i in range(indicator_count):
+                indicator_name = 'Number of people reached in fiscal year in <<insert program name>>'
                 i_created = self.create_indicator(indicator_name, dummy_program, top_level, 3)
                 if i_created:
                     try:
-                        metrics['indicator_created'][country_name] += 1
+                        metrics['indicator_created'][country] += 1
                     except KeyError:
-                        metrics['indicator_created'][country_name] = 1
+                        metrics['indicator_created'][country] = 1
                 else:
                     try:
-                        metrics['indicator_existed'][country_name] += 1
+                        metrics['indicator_existed'][country] += 1
                     except KeyError:
-                        metrics['indicator_existed'][country_name] = 1
+                        metrics['indicator_existed'][country] = 1
 
 
         print(f"{metrics['program_created']} programs were created in the dummy country, {metrics['program_existed']} already existed.")
@@ -130,36 +139,48 @@ class Command(BaseCommand):
             "Country/Regional Director - meeting strategic objectives.\n\n"
             "HQ - reporting\n\n")
 
-        indicator, created = Indicator.objects.get_or_create(
-            name=name,
-            program=program,
-            defaults={
-                'program': program,
-                'level': top_level,
-                'source': 'Mercy Corps',
-                'definition': definition_text,
-                'justification': indicator_justification,
-                'unit_of_measure': 'Participant',
-                'rationale_for_target': 'The target is based on the intended reach of the program',
-                'baseline': 0,
-                'direction_of_change': 2,
-                'data_points': '# of people reached',
-                'information_use': information_use,
-                'reporting_frequency_id': 7,
-            }
-        )
+        other_defaults = {
+            'level': top_level,
+            'source': 'Mercy Corps',
+            'definition': definition_text,
+            'justification': indicator_justification,
+            'unit_of_measure': 'Participant',
+            'rationale_for_target': 'The target is based on the intended reach of the program',
+            'baseline': 0,
+            'direction_of_change': 2,
+            'data_points': '# of people reached',
+            'information_use': information_use,
+            'reporting_frequency_id': 7,
+        }
 
-        indicator.indicator_type.add(IndicatorType.objects.get(indicator_type="Custom"))
-        if option_number == 1 and created:
-            indicator.target_frequency = 8
-            indicator.lop_target = 2
-            indicator.save()
-            PeriodicTarget.objects.get_or_create(
-                period='FY2021', target=1, customsort=0, indicator=indicator)
-            PeriodicTarget.objects.get_or_create(
-                period='FY2022', target=1, customsort=1, indicator=indicator)
 
-        if option_number == 3 and created:
+        if option_number == 1:
+            indicator, created = Indicator.objects.get_or_create(
+                name=name,
+                program=program,
+                defaults=other_defaults
+            )
+
+            indicator.indicator_type.add(IndicatorType.objects.get(indicator_type="Custom"))
+            if created:
+                indicator.target_frequency = 8
+                indicator.lop_target = 2
+                indicator.save()
+                PeriodicTarget.objects.get_or_create(
+                    period='FY2021', target=1, customsort=0, indicator=indicator)
+                PeriodicTarget.objects.get_or_create(
+                    period='FY2022', target=1, customsort=1, indicator=indicator)
+            return created
+
+        if option_number == 3:
+
+            indicator = Indicator.objects.create(
+                name=name,
+                program=program,
+                **other_defaults
+            )
+
+            indicator.indicator_type.add(IndicatorType.objects.get(indicator_type="Custom"))
             indicator.target_frequency = 3
             indicator.lop_target = 2
             indicator.save()
@@ -169,5 +190,16 @@ class Command(BaseCommand):
             PeriodicTarget.objects.get_or_create(
                 period='Year 2', target=1, customsort=1, indicator=indicator, start_date=date(2021, 7, 1),
                 end_date=date(2022, 6, 30))
+            return True
 
-        return created
+        raise NotImplementedError("Option must be 1 or 3")
+
+    @staticmethod
+    def clean_option3(country_names, name_template):
+        for program_name in [name_template.format(country_name) for country_name in country_names]:
+            try:
+                program = Program.objects.get(name=program_name)
+            except Program.DoesNotExist:
+                continue
+            for indicator in program.indicator_set.all():
+                indicator.delete()
