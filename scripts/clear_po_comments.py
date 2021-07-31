2e38f8a6c34e234032ac0c71e184a15b88999206
Sanjuro Jogdeo <sjogdeo@mercycorps.org>
2021-07-30 22:13:55 -0700

    For mercycorps/toladata#2467, clear out pofilter dross. Temp translations.

---
+++ scripts/clear_po_comments.py
@@ -0,0 +1,32 @@
+"""
+This script clears out the comments in all of the po files.  Used to clear pofilter spam
+that got into the files from an ill-advised use of pofilter.  If you're reading this, this file si probbly safe
+to delete
+"""
+import os
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Deletes comments from .po files.')
+    parser.add_argument('langdir', help='Relative directory path to a language in the locale directory')
+    parser.add_argument('--force', action='store_true', help='Without this flag, running the script will be a dry run.')
+    args = parser.parse_args()
+
+    for file_name in ['django', 'djangojs']:
+
+        with open(os.path.join(args.langdir, 'LC_MESSAGES', file_name + '.po'), 'r') as fh:
+            lines = fh.readlines()
+        print(len(lines))
+        with open(os.path.join(args.langdir, 'LC_MESSAGES', file_name + '_clean.po'), 'w') as fh:
+            for line in lines:
+                if line[:2] != '# ':
+                    fh.write(line)
+                else:
+                    print('Omitting', line)
+
+
+
+
+if __name__ == '__main__':
+    main()
