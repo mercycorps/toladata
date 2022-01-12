6d2b4f11738b9c3a3cc51d67af403564f53ed2ab
Sanjuro Jogdeo <sjogdeo@mercycorps.org>
2022-01-12 13:17:33 -0800

    For mercycorps/toladata#2647, modify permissions and get response.

--- indicators/tests/test_participant_count_indicators_results.py
+++ indicators/tests/test_participant_count_indicators_results.py
@@ -13,22 +13,44 @@ from workflow.models import PROGRAM_ROLE_CHOICES
 
 class TestParticipantCountSetup(test.TestCase):
     """
-    Test the get_level_depth method on the Level model to assure that the depth calculation is correct.
+    Test the views for participant count result creation and updates
     """
     def setUp(self):
-        self.tola_user = w_factories.TolaUserFactory()
-        self.program = w_factories.RFProgramFactory(country=[self.tola_user.country], tiers=True, levels=1)
+        self.country = w_factories.CountryFactory()
+        self.program = w_factories.RFProgramFactory(country=[self.country], tiers=True, levels=1)
+        self.tola_user = w_factories.TolaUserFactory(country=self.country)
+
         self.client = test.Client()
         IndicatorTypeFactory(indicator_type="Custom")
         ReportingFrequencyFactory(frequency="Annual")
 
-    def test_result_create_view(self):
-        self.client.force_login(self.tola_user.user)
-        w_factories.grant_program_access(self.tola_user, self.program, self.tola_user.country, PROGRAM_ROLE_CHOICES[2])
+    def has_correct_permission(self, tola_user, access_level, status_code):
+        self.client.force_login(tola_user.user)
+        w_factories.grant_program_access(
+            tola_user, self.program, self.country, access_level)
+        indicator = Indicator.objects.filter(admin_type=Indicator.ADMIN_PARTICIPANT_COUNT)[0]
+        response = self.client.get(reverse('pcountcreate', args=[indicator.pk]))
+        self.assertEqual(response.status_code, status_code)
+
+    def test_result_create_view_permissions(self):
         management.call_command(
             'create_participant_count_indicators', execute=True, create_disaggs_themes=True, suppress_output=True)
+        for access_level in [l[0] for l in PROGRAM_ROLE_CHOICES]:
+            tola_user = w_factories.TolaUserFactory(country=self.country)
+            w_factories.grant_program_access(
+                tola_user, self.program, self.country, access_level)
+            self.has_correct_permission(tola_user, access_level, 200)
+
+    def test_result_create_view_data(self):
+        management.call_command(
+            'create_participant_count_indicators', execute=True, create_disaggs_themes=True, suppress_output=True)
+        self.client.force_login(self.tola_user.user)
+        w_factories.grant_program_access(
+            self.tola_user, self.program, self.country, PROGRAM_ROLE_CHOICES[2][0])
         indicator = Indicator.objects.filter(admin_type=Indicator.ADMIN_PARTICIPANT_COUNT)[0]
         response = self.client.get(reverse('pcountcreate', args=[indicator.pk]))
         self.assertSetEqual(
             set(json.loads(response.content).keys()),
             {'outcome_themes', 'disaggregations', 'program_start_date', 'program_end_date'})
+
+
--- indicators/views/views_indicators.py
+++ indicators/views/views_indicators.py
@@ -130,14 +130,17 @@ def periodic_targets_form(request, program):
         'content': content,
     })
 
+
 @login_required
-@indicator_pk_adapter(has_indicator_write_access)
 @api_view(['GET', 'POST'])
 def participant_count_result_create_for_indicator(request, pk, *args, **kwargs):
     # pk is indicator.pk
-    if request.method == 'POST':
-        return JsonResponse({"message": "Got some data!", "data": request.data})
     indicator = get_object_or_404(Indicator, pk=pk)
+    if request.method == 'POST':
+        verify_program_access_level(request, indicator.program.pk, 'medium')
+        return JsonResponse({"message": "Got some data!", "data": request.data})
+
+    verify_program_access_level(request, indicator.program.pk, 'low')
     return_dict = {
         'outcome_themes': list(OutcomeTheme.objects.filter(is_active=True).values_list('pk', 'name')),
         **ParticipantCountIndicatorSerializer(indicator).data,
