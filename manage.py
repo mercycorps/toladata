31015284dae4a6844aa5b93bcaca2f03ac5972cd
Blake Long <blakemlong1@gmail.com>
2022-03-11 15:20:06 -0700

    added warning filtering to only get deprecation warnings for django 3.1

--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
+import warnings
+from django.utils.deprecation import RemovedInDjango31Warning
+
 
 if __name__ == "__main__":
 
     settings = 'tola.settings.test' if 'test' in sys.argv else 'tola.settings.local'
+
+    warnings.filterwarnings('default', category=RemovedInDjango31Warning)
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
