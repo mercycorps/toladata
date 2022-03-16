97349b47ece4fbe52c465755fbb5c76d6aa6bd10
Blake Long <blakemlong1@gmail.com>
2022-03-16 10:22:10 -0600

    Upgraded Django to 3.1

--- manage.py
+++ manage.py
@@ -2,14 +2,14 @@
 import os
 import sys
 import warnings
-from django.utils.deprecation import RemovedInDjango31Warning
+from django.utils.deprecation import RemovedInDjango40Warning
 
 
 if __name__ == "__main__":
 
     settings = 'tola.settings.test' if 'test' in sys.argv else 'tola.settings.local'
 
-    warnings.filterwarnings('default', category=RemovedInDjango31Warning)
+    warnings.filterwarnings('ignore', category=RemovedInDjango40Warning)
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
