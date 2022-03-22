83ecd41886f01abfa83cb39ea8173161f3832fc6
Blake Long <blakemlong1@gmail.com>
2022-03-22 10:18:01 -0600

    removed warning filters

--- manage.py
+++ manage.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-import warnings
-from django.utils.deprecation import RemovedInDjango40Warning
-
 
 if __name__ == "__main__":
 
     settings = 'tola.settings.test' if 'test' in sys.argv else 'tola.settings.local'
-
-    warnings.filterwarnings('ignore', category=RemovedInDjango40Warning)
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
