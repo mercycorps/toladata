e3ebc9019239d721069dad4cfdd0d625b04e2e3f
Karen Barkemeyer <karenbarkemeyer@gmail.com>
2022-06-02 14:10:51 -0700

    Expand participant count results tests

--- indicators/tests/test_participant_count_indicators_results.py
+++ indicators/tests/test_participant_count_indicators_results.py
@@ -18,10 +18,11 @@ class TestParticipantCountSetup(test.TestCase):
     """
     Test the views for participant count result creation and updates
     """
+    today = date.today()
+
     def setUp(self):
-        today = date.today()
-        program_start = date(today.year, today.month, 1)
-        program_end = date(today.year + 1, 6, 30)
+        program_start = date(self.today.year, self.today.month, 1)
+        program_end = date(self.today.year + 1, 6, 30)
         self.country = w_factories.CountryFactory()
         self.program = w_factories.RFProgramFactory(
             country=[self.country], tiers=True, levels=1,
@@ -32,11 +33,20 @@ class TestParticipantCountSetup(test.TestCase):
         IndicatorTypeFactory(indicator_type=IndicatorType.PC_INDICATOR_TYPE)
         ReportingFrequencyFactory(frequency=ReportingFrequency.PC_REPORTING_FREQUENCY)
 
-        first_fiscal_year = today.year if today.month < 7 else today.year + 1
+        first_fiscal_year = self.today.year if self.today.month < 7 else self.today.year + 1
         self.period_string = 'FY' + str(first_fiscal_year)
         self.pt_start = str(date(first_fiscal_year - 1, 7, 1))
         self.pt_end = str(date(first_fiscal_year, 6, 30))
         self.view_only = False
+
+    def create_indicator_utils(self):
+        management.call_command(
+            'create_participant_count_indicators', execute=True, create_disaggs_themes=True, suppress_output=True)
+        self.client.force_login(self.tola_user.user)
+        w_factories.grant_program_access(
+            self.tola_user, self.program, self.country, PROGRAM_ROLE_CHOICES[2][0])
+        indicator = Indicator.objects.filter(admin_type=Indicator.ADMIN_PARTICIPANT_COUNT)[0]
+        return indicator
 
     def has_correct_permission(self, tola_user, access_level, status_code):
         self.client.force_login(tola_user.user)
@@ -77,6 +87,36 @@ class TestParticipantCountSetup(test.TestCase):
             result_data['disaggregations'].append(disagg_dict)
         return result_data
 
+    def update_base_results(self, base_result_data):
+        # Adding disaggregation values to base_result_data, collect values in list to compare to result object
+        updated_disagg_values = []
+        for disagg in base_result_data['disaggregations']:
+            if disagg['disaggregation_type'] == 'SADD (including unknown) with double counting':
+                disagg['labels'][1]['value'] = 1
+                updated_disagg_values.append(1.00)
+            if disagg['disaggregation_type'] == 'Actual with double counting':
+                disagg['labels'][0]['value'] = 1
+                updated_disagg_values.append(1.00)
+                disagg['labels'][1]['value'] = 1
+                updated_disagg_values.append(1.00)
+        return base_result_data, updated_disagg_values
+
+    def create_result(self, pt, indicator):
+        result = ResultFactory(
+            periodic_target=pt,
+            indicator=indicator,
+            program=self.program,
+            date_collected=self.today
+        )
+        return result
+
+    def get_outcome_theme_list(self, result):
+        ot_object = result.outcome_themes.filter(is_active=True)
+        ot_list = []
+        for ot in ot_object:
+            ot_list.append(getattr(ot, 'id'))
+        return ot_list
+
     def test_result_create_view_permissions(self):
         management.call_command(
             'create_participant_count_indicators', execute=True, create_disaggs_themes=True, suppress_output=True)
@@ -87,82 +127,80 @@ class TestParticipantCountSetup(test.TestCase):
             self.has_correct_permission(tola_user, access_level, 200)
 
     def test_result_create_view_data(self):
-        management.call_command(
-            'create_participant_count_indicators', execute=True, create_disaggs_themes=True, suppress_output=True)
-        self.client.force_login(self.tola_user.user)
-        w_factories.grant_program_access(
-            self.tola_user, self.program, self.country, PROGRAM_ROLE_CHOICES[2][0])
-        indicator = Indicator.objects.filter(admin_type=Indicator.ADMIN_PARTICIPANT_COUNT)[0]
+        indicator = self.create_indicator_utils()
         response = self.client.get(reverse('pcountcreate', args=[indicator.pk]))
         data = json.loads(response.content)
+
         self.assertSetEqual(
             set(data.keys()),
-            {'outcome_themes', 'disaggregations', 'program_start_date', 'program_end_date', 'periodic_target', 'pt_start_date', 'pt_end_date'})
+            {'outcome_themes', 'disaggregations', 'program_start_date', 'program_end_date', 'periodic_target',
+             'pt_start_date', 'pt_end_date'})
         self.assertEqual(data['pt_start_date'], self.pt_start)
         self.assertEqual(data['pt_end_date'], self.pt_end)
 
-    def test_result_update_data(self):
-        management.call_command(
-            'create_participant_count_indicators', execute=True, create_disaggs_themes=True, suppress_output=True)
-        self.client.force_login(self.tola_user.user)
-        w_factories.grant_program_access(
-            self.tola_user, self.program, self.country, PROGRAM_ROLE_CHOICES[2][0])
-        indicator = Indicator.objects.filter(admin_type=Indicator.ADMIN_PARTICIPANT_COUNT)[0]
+    def test_result_create_view(self):
+        indicator = self.create_indicator_utils()
+        # Get base result data and update it
+        base_result_data = self.get_base_result_data(indicator)
+        updated_base_result_data, updated_disagg_values = self.update_base_results(base_result_data)
+        disaggs = updated_base_result_data['disaggregations']
+        outcome_theme = [base_result_data['outcome_themes'].last()['id']]
+        payload = {'disaggregations': disaggs, 'outcome_theme': outcome_theme,
+                   'date_collected': date.today(), 'indicator': indicator.pk}
+        response_post = self.client.post(reverse('pcountcreate', args=[indicator.pk]), data=payload,
+                                         content_type='application/json')
+        updated_result = Result.objects.filter(indicator=indicator.pk).first()
+        outcome_themes_list = self.get_outcome_theme_list(updated_result)
+        disagg_values = []
+        for disagg in updated_result.disaggregated_values:
+            disagg_values.append(disagg.value)
+
+        self.assertEqual(response_post.status_code, 200)
+        self.assertEqual(outcome_themes_list, outcome_theme)
+        self.assertEqual(disagg_values, updated_disagg_values)
+
+
+    def test_result_update(self):
+        indicator = self.create_indicator_utils()
         # First fiscal year periodic target for pc indicator
         pt = indicator.periodictargets.all().first()
-        today = date.today()
-        result = ResultFactory(
-            periodic_target=pt,
-            indicator=indicator,
-            program=self.program,
-            date_collected=today
-        )
-
+        result = self.create_result(pt, indicator)
         base_result_data = self.get_base_result_data(indicator)
-
         # Add outcome theme 'Economic Opportunity'
         result.outcome_themes.add(base_result_data['outcome_themes'].first()['id'])
-
         response_get = self.client.get(reverse('pcountupdate', args=[result.pk]))
         data = json.loads(response_get.content)
 
         self.assertEqual(response_get.status_code, 200)
         self.assertEqual(data['periodic_target']['period'], self.period_string)
-        self.assertEqual(data['date_collected'], str(today))
-        self.assertEqual(data['outcome_themes'][0][2], True)
+        self.assertEqual(data['date_collected'], str(self.today))
+        self.assertTrue(data['outcome_themes'][0][2])
         self.assertEqual(data['view_only'], self.view_only)
         self.assertEqual(data['pt_start_date'], self.pt_start)
         self.assertEqual(data['pt_end_date'], self.pt_end)
 
-        # Adding disaggregation values to base_result_data
-        for disagg in base_result_data['disaggregations']:
-            if disagg['disaggregation_type'] == 'SADD (including unknown) with double counting':
-                disagg['labels'][1]['value'] = 1
-            if disagg['disaggregation_type'] == 'Actual with double counting':
-                disagg['labels'][0]['value'] = 1
-                disagg['labels'][1]['value'] = 1
-
-        disaggs = base_result_data['disaggregations']
+        updated_base_result_data, updated_disagg_values = self.update_base_results(base_result_data)
+        disaggs = updated_base_result_data['disaggregations']
         # Adding a second outcome theme to the existing one
         outcome_theme = [base_result_data['outcome_themes'].last()['id']]
         # Pushing result date to tomorrow
-        tomorrow = date(today.year, today.month, today.day + 1)
+        tomorrow = date(self.today.year, self.today.month, self.today.day + 1)
         url = reverse('pcountupdate', args=[result.pk])
         payload = {'disaggregations': disaggs, 'outcome_theme': outcome_theme,
                    'date_collected': tomorrow, 'rationale': 'test'}
-
         response_post = self.client.put(url, data=payload, content_type='application/json')
         # Getting updated result object
         updated_result = Result.objects.get(pk=result.pk)
         # Creating list of expected outcome themes
         outcome_theme_list = [base_result_data['outcome_themes'].first()['id']] + outcome_theme
         # Fetching outcome themes from updated result object and adding them to list
-        ot_object = result.outcome_themes.filter(is_active=True)
-        ot_list = []
-        for ot in ot_object:
-            ot_list.append(getattr(ot, 'id'))
+        ot_list = self.get_outcome_theme_list(updated_result)
+        disagg_values = []
+        for disagg in updated_result.disaggregated_values:
+            disagg_values.append(disagg.value)
 
         self.assertEqual(response_post.status_code, 200)
         self.assertEqual(updated_result.periodic_target.period, self.period_string)
         self.assertEqual(updated_result.date_collected, tomorrow)
         self.assertEqual(ot_list, outcome_theme_list)
+        self.assertEqual(disagg_values, updated_disagg_values)
