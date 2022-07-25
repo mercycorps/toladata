963f0e4c599629d41b32187a21738efdc6474e65
Karen Barkemeyer <karenbarkemeyer@gmail.com>
2022-07-25 11:01:53 -0700

    Add ProgramAdminSerializer fields

--- tola_management/tests/test_programadmin_viewset.py
+++ tola_management/tests/test_programadmin_viewset.py
@@ -207,7 +207,7 @@ class TestProgramFieldsStressTest(test.TestCase):
 
     def get_data(self, program_qs):
         # 3 queries: program with annotations, prefetched sectors, prefetched countries
-        with self.assertNumQueries(7):
+        with self.assertNumQueries(8):
             return ProgramAdminSerializer(program_qs, many=True).data[0]
 
     def get_users_filtered_data(self, program_pk):
@@ -276,7 +276,7 @@ class TestProgramFieldsStressTest(test.TestCase):
         self.assertEqual(users_data['count'], 14)
 
     def test_query_count_on_multiple(self):
-        with self.assertNumQueries(8):
+        with self.assertNumQueries(9):
             program_qs = ProgramAdminViewSet.base_queryset().all()
             data = ProgramAdminSerializer(program_qs, many=True).data
             self.assertEqual(len(data), 25)
