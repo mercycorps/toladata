80e9d3b1641085e08b91017e31ef903e7ed1aaee
Paul Souders <paul@axoplasm.com>
2022-08-17 09:45:35 -0700

    ProgramAuditLog
    for mercycorps/TolaActivity#2822

--- tola_management/admin.py
+++ tola_management/admin.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from tola_management.models import (
+    ProgramAuditLog,
     UserManagementAuditLog,
 )
 
@@ -13,4 +14,13 @@ class UserManagementAuditLogAdmin(admin.ModelAdmin):
     readonly_fields = ('date',)
     list_display = ('date', 'admin_user', 'modified_user', 'change_type')
     search_fields = ('admin_user', 'modified_user')
+    list_filter = ('change_type',)
+
+
+@admin.register(ProgramAuditLog)
+class ProgramAuditLogAdmin(admin.ModelAdmin):
+    autocomplete_fields = ('program', 'user', 'indicator', 'level')
+    readonly_fields = ('rationale_selections', 'date')
+    list_display = ('date', 'change_type', 'user', 'program', 'indicator', 'level')
+    search_fields = ('user__name', 'program__name', 'indicator__name', 'level__name')
     list_filter = ('change_type',)
